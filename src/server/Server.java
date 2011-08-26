7bd876ecafa1f24d8fef19d947332883ee3eea6e
sdemircan <demircan.serhat@gmail.com>
2011-08-26 21:01:43 +0300

    invokeMethod overloadded.

--- src/server/Server.java
+++ src/server/Server.java
@@ -14,18 +14,14 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-
-
-
-
 public class Server {
 
 	private ServerSocket server;
 	private ConcurrentHashMap<String, Player> playerTable;
 	private ExecutorService playerPool;
-	
+
 	private Log log;
-	
+
 	private int port;
 
 	public Server(int port) {
@@ -40,8 +36,8 @@ public class Server {
 		Socket socket;
 		try {
 			server = new ServerSocket(port);
-			log.log(String.format("Server running on %s and %d. port.", 
-					server.getInetAddress().getHostAddress(), port));
+			log.log(String.format("Server running on %s and %d. port.", server
+					.getInetAddress().getHostAddress(), port));
 			while (true) {
 				socket = server.accept();
 				Player player = new Player(socket);
@@ -90,66 +86,92 @@ public class Server {
 
 		@Override
 		public void run() {
-			String message;
+			String inputMessage;
 			String params[];
-
-			while (!nickAccepted) {
-				message = input.nextLine();
-				params = message.split(" ");
-				if (params.length == 3 && params[0].equals("NICK")
-						&& params[1].equals("SET")){
-					cmdNICKSET(params[2]);
+			try {
+				while (!nickAccepted) {
+					inputMessage = input.nextLine();
+					params = inputMessage.split(" ");
+					if (params.length == 3 && params[0].equals("NICK")
+							&& params[1].equals("SET")) {
+						cmdNICKSET(params[2]);
+					}
 				}
-			}
 
-			while ((message = input.nextLine()) != null) {
-				handleMessage(nick, message);
+				while ((inputMessage = input.nextLine()) != null) {
+					handleMessage(nick, inputMessage);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				cmdDISCONNECT(nick);
 			}
 
 		}
-		private void cmdNICKSET(String nick) {
-			if(hasNick(nick)){
+
+		private void cmdNICKSET(String nick) throws IOException {
+			if (hasNick(nick)) {
 				cmdERR(output, "Nick already in use");
 				return;
 			}
+
 			nickAccepted = true;
 			this.nick = nick;
+
 			playerTable.put(this.nick, this);
 			cmdNICKACCEPTED(output, nick);
 			cmdMSG("SERVER", String.format("%s connected to server.", nick));
+
 			log.log(String.format("%s connectted to server.", nick));
 		}
-	
+
 	}
-	
-	//METHODS
-	
-	private boolean hasNick(String nick){
+
+	// METHODS
+
+	private boolean hasNick(String nick) {
 		return playerTable.containsKey(nick);
 	}
-	
-	private void invokeMethod(String command, String from, String param) throws Exception{
+
+	private void invokeMethod(String command, String from) throws Exception {
 		String name = "cmd" + command;
-		Method method = this.getClass().getMethod(name, String.class, String.class);
-		method.invoke(getClass(), param);
+		Method method = this.getClass().getMethod(name, String.class);
+		method.invoke(this, from);
 	}
-	
+
+	private void invokeMethod(String command, String from, String param)
+			throws Exception {
+		String name = "cmd" + command;
+		Method method = this.getClass().getMethod(name, String.class,
+				String.class);
+		method.invoke(this, from, param);
+	}
+
 	private void handleMessage(String nick, String message) {
 		String params[] = message.split(" ", 1);
 		try {
-			invokeMethod(params[0], nick, params[1]);
+			if (params.length != 1) {
+				invokeMethod(params[0], nick, params[1]);
+			} else {
+				invokeMethod(params[0], nick);
+			}
 		} catch (Exception e) {
 			cmdERR(nick, "Method invoke error");
 		}
-		
+
 	}
-	
-	private void cmdNICKACCEPTED(Formatter output, String nick){
+
+	private void cmdDISCONNECT(String player) {
+		playerTable.remove(player);
+	}
+
+	private void cmdNICKACCEPTED(Formatter output, String nick) {
 		output.format("NICK ACCEPTED %s", nick);
 		output.flush();
 	}
+
 	private void cmdMSG(String from, String param) {
-		
+
 		Iterator<Player> it = playerTable.values().iterator();
 		while (it.hasNext()) {
 			Formatter formatter = it.next().output;
@@ -157,22 +179,22 @@ public class Server {
 			formatter.flush();
 		}
 	}
-	
-	private void cmdNAMES(String to, String param){
+
+	private void cmdNAMES(String to) {
 		Formatter output = getOutput(to);
 		String names = null;
-		
-		Enumeration<String> keys= playerTable.keys();
-		
-		while(keys.hasMoreElements()){
+
+		Enumeration<String> keys = playerTable.keys();
+
+		while (keys.hasMoreElements()) {
 			names += keys.nextElement() + ",";
 		}
-		
+
 		output.format("NAMES %s", names);
 		output.flush();
 	}
-	
-	private void cmdPRIVMSG(String from, String param){
+
+	private void cmdPRIVMSG(String from, String param) {
 		String params[] = param.split(" ", 3);
 		String to = params[0];
 		String message = params[2];
@@ -180,25 +202,20 @@ public class Server {
 		output.format("PRIVMSG %s %s %s", to, from, message);
 		output.flush();
 	}
-	
-	private void cmdERR(String to, String message){
+
+	private void cmdERR(String to, String message) {
 		Formatter output = getOutput(to);
 		output.format("ERR %s", message);
 		output.flush();
 	}
-	
-	private void cmdERR(Formatter output, String message){
+
+	private void cmdERR(Formatter output, String message) {
 		output.format("ERR %s", message);
 		output.flush();
 	}
-	
-	private Formatter getOutput(String nick){
+
+	private Formatter getOutput(String nick) {
 		return playerTable.get(nick).output;
 	}
-	
-	
-	
-
-	
 
 }
