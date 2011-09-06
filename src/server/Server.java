8eed724d4cc23d0d71d65cef47830fcc1d161b51
sdemircan <demircan.serhat@gmail.com>
2011-09-06 15:12:33 +0300

    JOIN methodu eklendi.

--- src/server/Server.java
+++ src/server/Server.java
@@ -23,19 +23,16 @@ import java.util.concurrent.Executors;
 public class Server {
 
 	private ServerSocket server;
-	
+
 	private ConcurrentHashMap<String, Player> playerTable;
 	private ExecutorService playerPool;
-	
-	//TODO:Oyun kuyrukları eklenecek.
-	private List chessQueue;
-	
+
+	// TODO:Oyun kuyrukları eklenecek.
+	private List<Player> chessQueue;
+
 	//
-	private ConcurrentHashMap<String, List> gameTable;
-	
-	
-	
-	
+	private ConcurrentHashMap<String, List<Player>> gameTable;
+
 	private Log log;
 
 	private int port;
@@ -47,24 +44,24 @@ public class Server {
 		initializeLog();
 		initializeQueues();
 	}
-	
-	public void initializeLog(){
+
+	public void initializeLog() {
 		log = new Log();
 		log.addPrintStream(System.out);
 	}
-	
-	public void initializeQueues(){
+
+	public void initializeQueues() {
 		this.chessQueue = new LinkedList<Player>();
-		gameTable = new ConcurrentHashMap<String, List>();
+		gameTable = new ConcurrentHashMap<String, List<Player>>();
 		gameTable.put("Chess", chessQueue);
 	}
-	
+
 	public void run() {
 		Socket socket;
 		try {
 			server = new ServerSocket(port);
-			log.log(String.format("%s - Server running on %s and %d. port.",getDate(), server
-					.getInetAddress().getHostAddress(), port));
+			log.log(String.format("%s - Server running on %s and %d. port.",
+					getDate(), server.getInetAddress().getHostAddress(), port));
 			while (true) {
 				socket = server.accept();
 				Player player = new Player(socket);
@@ -128,15 +125,16 @@ public class Server {
 				}
 
 				while ((inputMessage = input.nextLine()) != null) {
-					log.log(String.format("%s - %s requested %s", getDate(), nick, inputMessage));
+					log.log(String.format("%s - %s requested %s", getDate(),
+							nick, inputMessage));
 					handleMessage(nick, inputMessage);
 				}
 			} catch (IOException e) {
 				closeConnection();
 				log.log(String.format("%s - %s DISCONNECTED.", getDate(), nick));
-			} catch (IllegalStateException e){
+			} catch (IllegalStateException e) {
 				log.log(String.format("%s - %s DISCONNECTED.", getDate(), nick));
-			}catch (NoSuchElementException e){
+			} catch (NoSuchElementException e) {
 				cmdDISCONNECT(nick);
 			}
 
@@ -166,7 +164,8 @@ public class Server {
 			cmdMSG("SERVER",
 					String.format("SERVER %s connected to server.", nick));
 
-			log.log(String.format("%s - %s connectted to server.", getDate(), nick));
+			log.log(String.format("%s - %s connectted to server.", getDate(),
+					nick));
 		}
 
 	}
@@ -210,8 +209,17 @@ public class Server {
 		}
 
 	}
-	
-	private void cmdGAMES(String nick){
+
+	private void cmdJOIN(String from, String game) {
+		Player player = playerTable.get(from);
+		gameTable.get(game).add(player);
+
+		// TODO burada oyunun yeterli kişi sayısına ulaşıp ulaşmadığı kontrol
+		// edilmeli.
+
+	}
+
+	private void cmdGAMES(String nick) {
 		Formatter output = getOutput(nick);
 		String games = "";
 
@@ -224,7 +232,7 @@ public class Server {
 		output.format("GAMES %s\n", games);
 		output.flush();
 	}
-	
+
 	private void cmdDISCONNECT(String nick) {
 		playerTable.get(nick).closeConnection();
 		playerTable.remove(nick);
@@ -265,13 +273,13 @@ public class Server {
 			cmdERR(from, to + " wasn't found");
 		}
 		String message = params[2];
-		
-		//Mesajı Alacak Kisiye
+
+		// Mesajı Alacak Kisiye
 		Formatter output = getOutput(to);
 		output.format("PRIVMSG %s %s %s\n", to, from, message);
 		output.flush();
-		
-		//Mesajı Yollayana
+
+		// Mesajı Yollayana
 		output = getOutput(from);
 		output.format("PRIVMSG %s %s %s\n", to, from, message);
 		output.flush();
@@ -291,15 +299,15 @@ public class Server {
 	private Formatter getOutput(String nick) {
 		return playerTable.get(nick).output;
 	}
-	
+
 	private String getDate() {
 		String date = new Date().toString();
 		int index = date.indexOf(':');
 		date = '[' + date.substring(index - 2, index + 6) + ']';
 		return date;
 	}
-	
-	public static void main(String args[]){
+
+	public static void main(String args[]) {
 		Server server = new Server(5556);
 		server.run();
 	}
